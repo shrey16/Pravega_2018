8b613e33b92f1b227600bea09b1c244380867cf6
Tamoghna Chowdhury <chowtam8991@gmail.com>
2017-10-20 20:52:01 +0530

    Add CSV and XLSX export support to dbscraper

--- cultural/registrations/dbscraper.py
+++ cultural/registrations/dbscraper.py
@@ -1,6 +1,52 @@
 import sys
+import io
+import os
+import csv
 import json
 from .models import *
+
+try:
+    from openpyxl import *
+
+    def csvtoxlsx(csv_name, xlsx_name, floats=None, csv_dialect='excel-tab', quoting=csv.QUOTE_ALL):
+        """
+        A function to convert a CSV file to XLSX so it can be used by openpyxl.
+        csvname = path of the csv you want to convert (include .csv)
+        xlsx_name = path of the resulting xlsx file (include .xlsx)
+        floats = A list of column indexes in which numbers appear
+        """
+
+        f = open(csv_name, 'rt')
+        reader = csv.reader(f, dialect=csv_dialect, quoting=csv.QUOTE_ALL)
+        wb = Workbook()
+        dest_filename = xlsx_name
+        ws = wb.worksheets[0]
+        ws.title = os.path.basename(os.path.splitext(xlsx_name)[0])  # chop off extension
+        if floats is None:
+            floats = []
+        for row_index, row in enumerate(reader):
+            for column_index, cell in enumerate(row):
+                if column_index in floats:
+                    s = cell
+                    # Handles heading row or non floats
+                    try:
+                        s = float(s)
+                        ws.cell(column=column_index+1, row=row_index+1).value = s
+
+                    except ValueError:
+                        ws.cell(column=column_index+1, row=row_index+1).value = s
+
+                elif column_index not in floats:
+                    # Handles openpyxl 'illegal chars'
+                    try:
+                        ws.cell(column=column_index+1, row=row_index+1).value = cell
+
+                    except:
+                        ws.cell(column=column_index+1, row=row_index+1).value = 'illegal char'
+
+        wb.save(filename=dest_filename)
+except ImportError:
+    pass
 
 
 class RegistrationData:
@@ -8,16 +54,66 @@ class RegistrationData:
     def load_from_db(self):
         pass
 
-    def dump_to_json(self):
-        raw_data = list(map(lambda x: {'serial_no': x[0], 'entry': x[
-                        1].__dict__}, enumerate(self.load_from_db())))
+    def dump_data(self):
+        raw_data = [{'serial_no': i, 'entry': x.__dict__}
+                    for i, x in enumerate(self.load_from_db())]
         data = {}
         data['total_entries'] = len(raw_data)
         data['entries'] = raw_data
-        return json.dumps(data, indent=4)
+        return data
 
-    def save_all_data_as_json(self, file=sys.stdout):
-        print(self.dump_to_json(), file=file)
+    def prettify(self, item):
+        return str(item).replace('_', ' ').title()
+
+    def dump_to_csv(self):
+        data = self.dump_data()
+        output = io.StringIO()
+        csv_writer = csv.writer(
+            output, dialect='excel-tab', quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(["Total Entries", data['total_entries']])
+        for entry in data['entries']:
+            csv_writer.writerow(["Serial No.", entry['serial_no']])
+            csv_writer.writerow(["Entry"])
+            entry_data = entry['entry']
+
+            entry_without_participants_or_accompanists = {
+                self.prettify(key): value
+                for key, value in entry_data.items()
+                if not (key == 'participants' or key == 'accompanists')}
+            csv_writer.writerow(
+                list(entry_without_participants_or_accompanists.keys()))
+            csv_writer.writerow(
+                list(entry_without_participants_or_accompanists.values()))
+
+            entry_with_only_participants = entry_data['participants']
+            csv_writer.writerow(["Participants"])
+            for participant in entry_with_only_participants:
+                csv_writer.writerow([self.prettify(key)
+                                     for key in participant.keys()])
+                csv_writer.writerow(
+                    list(participant.values()))
+
+            if 'accompanists' in entry_data:
+                entry_with_only_accompanists = entry_data['accompanists']
+                csv_writer.writerow(["Accompanists"])
+                for accompanist in entry_with_only_accompanists:
+                    csv_writer.writerow([self.prettify(key)
+                                         for key in accompanist.keys()])
+                    csv_writer.writerow(
+                        list(accompanist.values()))
+        final_output = output.getvalue()
+        output.close()
+        return final_output
+
+    def dump_to_json(self):
+        return json.dumps(self.dump_data(), indent=4)
+
+    def save_all_data_to_file(self, path, json=False):
+        with open(path, 'w') as output:
+            self.save_all_data(json=json, file=output)
+
+    def save_all_data(self, json=False, file=sys.stdout):
+        print(self.dump_to_json() if json else self.dump_to_csv(), file=file)
 
 
 class ProsceniumTheatreRegistrationData(RegistrationData):
