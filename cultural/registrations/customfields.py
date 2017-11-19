d942898d76fa8265d9d43bceaef79e611a669490
Tamoghna Chowdhury <chowtam8991@gmail.com>
2017-11-19 14:08:01 +0530

    Update phone number error messages

--- cultural/registrations/customfields.py
+++ cultural/registrations/customfields.py
@@ -5,11 +5,15 @@ from django.core.validators import RegexValidator
 
 class PhoneNumberField():
     phone_regex = r'^\+91\d{10}$|^0\d{10}$|^\d{10}$'
-    message = "Phone number must be entered in the format: '+91----------'. 12 digits allowed."
+    message = "Please enter a valid 10 digit phone number, prefixed by either 0, +91, or nothing"
+    error_messages = {
+        'required': 'This field is required',
+        'invalid': message
+    }
 
     def get_field(as_regexField=False, *args, **kwargs):
         if as_regexField:
-            return forms.RegexField(regex=PhoneNumberField.phone_regex, help_text=PhoneNumberField.message, *args, **kwargs)
+            return forms.RegexField(regex=PhoneNumberField.phone_regex, help_text=PhoneNumberField.message, error_messages=PhoneNumberField.error_messages, *args, **kwargs)
         else:
             return models.CharField(validators=[RegexValidator(regex=PhoneNumberField.phone_regex,
                                                                message=PhoneNumberField.message)], max_length=15, blank=True)
